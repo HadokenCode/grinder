672d04fff953cf1beeabffd23f0611d5dab7def1
Lark Mullins <larkmullins@gmail.com>
2017-10-11 01:05:48 -0400

    cors config and default config setup

--- middleware/cors.go
+++ middleware/cors.go
@@ -4,6 +4,20 @@ import (
 	"github.com/rinkbase/grinder"
 )
 
+// CORSConfig configuration for CORS middleware
+type CORSConfig struct {
+	AllowOrigins   []string `json:"allow_origins"`
+	AllowMethods   []string `json:"allow_methods"`
+	AllowHeaders   []string `json:"allow_headers"`
+	HeadersExposed []string `json:"headers_exposed"`
+}
+
+// DefaultCORSConfig handles the default CORS configuration for grinder
+var DefaultCORSConfig = CORSConfig{
+	AllowOrigins: []string{"*"},
+	AllowMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+}
+
 // CORSError returns a grinder Handler when an error is occured
 func CORSError(ctx grinder.Context) error {
 	return ctx.JSON(500, "CORS Error")

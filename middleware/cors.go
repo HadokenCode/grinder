d340e51be57f0cf0c984e8d6a68c0e1df7ee8c3e
Lark Mullins <larkmullins@gmail.com>
2017-10-13 00:24:36 -0400

    functional cors middleware - wip

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,20 +2,24 @@ package middleware
 
 import (
 	"github.com/rinkbase/grinder"
+	"net/http"
+	"strings"
 )
 
 // CORSConfig configuration for CORS middleware
 type CORSConfig struct {
-	AllowOrigins   []string `json:"allow_origins"`
-	AllowMethods   []string `json:"allow_methods"`
-	AllowHeaders   []string `json:"allow_headers"`
-	HeadersExposed []string `json:"headers_exposed"`
+	AllowedOrigins []string `json:"allowed_origins"`
+	AllowedMethods []string `json:"allowed_methods"`
+	AllowedHeaders []string `json:"allowed_headers"`
+	ExposedHeaders []string `json:"exposed_headers"`
 }
 
 // DefaultCORSConfig handles the default CORS configuration for grinder
 var DefaultCORSConfig = CORSConfig{
-	AllowOrigins: []string{"*"},
-	AllowMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+	AllowedOrigins: []string{"*"},
+	AllowedMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+	AllowedHeaders: []string{"*"},
+	ExposedHeaders: []string{"*"},
 }
 
 // CORSError returns a grinder Handler when an error is occured
@@ -25,5 +29,53 @@ func CORSError(ctx grinder.Context) error {
 
 // CORS middleware for Grinder routes
 func CORS(ctx grinder.Context, handler grinder.Handler) grinder.Handler {
-	return CORSError
+	return CORSConfigured(ctx, handler, DefaultCORSConfig)
+}
+
+// CORSConfigured returns a configured CORS middleware
+func CORSConfigured(ctx grinder.Context, handler grinder.Handler, config CORSConfig) grinder.Handler {
+	if len(config.AllowedOrigins) == 0 {
+		config.AllowedOrigins = DefaultCORSConfig.AllowedOrigins
+	}
+
+	if len(config.AllowedMethods) == 0 {
+		config.AllowedMethods = DefaultCORSConfig.AllowedMethods
+	}
+
+	if len(config.AllowedHeaders) == 0 {
+		config.AllowedHeaders = DefaultCORSConfig.AllowedHeaders
+	}
+
+	if len(config.ExposedHeaders) == 0 {
+		config.ExposedHeaders = DefaultCORSConfig.ExposedHeaders
+	}
+
+	allowedOrigins := strings.Join(config.AllowedOrigins, ",")
+	allowedMethods := strings.Join(config.AllowedMethods, ",")
+	allowedHeaders := strings.Join(config.AllowedHeaders, ",")
+	exposedHeaders := strings.Join(config.ExposedHeaders, ",")
+
+	return func(ctx grinder.Context) error {
+		req := ctx.Request()
+		res := ctx.Response()
+
+		if req.Method != "OPTIONS" {
+			res.Header().Add("Vary", "Origin")
+			res.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
+			if exposedHeaders != "" {
+				res.Header().Set("Access-Control-Expose-Headers", exposedHeaders)
+			}
+
+			return handler(ctx)
+		}
+
+		res.Header().Add("Vary", "Origin")
+		res.Header().Add("Vary", "Access-Control-Request-Method")
+		res.Header().Add("Vary", "Access-Control-Request-Headers")
+		res.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
+		res.Header().Set("Access-Control-Allow-Methods", allowedMethods)
+		res.Header().Set("Access-Control-Allow-Headers", allowedHeaders)
+
+		return ctx.Code(http.StatusNoContent)
+	}
 }

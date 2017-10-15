1dee78440048e0434e5cb0cc9adb1994a9dc8bc9
Lark Mullins <larkmullins@gmail.com>
2017-10-14 23:50:46 -0400

    added initial CORS testing

---
+++ middleware/cors_test.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/rinkbase/grinder"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCORS(t *testing.T) {
+	g := grinder.New()
+
+	req := httptest.NewRequest("GET", "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := g.NewContext(rec, req)
+	nfh := grinder.NotFoundHandler(ctx)
+
+	assert.True(t, reflect.TypeOf(nfh).String() == "*grinder.ErrorHTTP")
+}
